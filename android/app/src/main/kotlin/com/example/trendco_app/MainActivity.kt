e590f6a4f712bcfec7cc7f049265caaf60ba4272
pavlysamir <bavlysamir33@gmail.com>
2025-02-24 20:42:36 +0200

    add packages in dependences

---
+++ android/app/src/main/kotlin/com/example/trendco_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.trendco_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:trendco_app/trendco_app.dart';
+
+void main() {
+  runApp(const TrendcoApp());
+}
